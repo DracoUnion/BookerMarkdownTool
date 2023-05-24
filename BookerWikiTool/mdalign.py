4bef9db217445b2c67ec3f869239b58354453bc5
wizardforcel <562826179@qq.com>
2023-05-24 21:46:50 +0800

    2023-05-24 21:46:50

--- BookerWikiTool/mdalign.py
+++ BookerWikiTool/mdalign.py
@@ -1,5 +1,4 @@
 import re
-import sys
 from os import path
 import yaml
 import json
@@ -121,49 +120,6 @@ def make_align(md1, md2):
         idx2 += 1
     return res
     
-def extreact_pre(md):
-    pres = []
-    def repl_func(m):
-        s = m.group()
-        pres.append(s)
-        idx = len(pres) - 1
-        return f'[PRE{idx}]'
-    md = re.sub(RE_PRE, repl_func, md)
-    return md, pres
-    
-def recover_pre(md, pres):
-    for i, pre in enumerate(pres):
-        md = md.replace(f'[PRE{i}]', pre)
-    return md
-    
-def extract_pre_handler(args):
-    fname = args.fname
-    if not fname.endswith('.md'):
-        print('请提供MD文件')
-        return
-    json_fname = fname + '.json'
-    if path.isfile(json_fname):
-        print('文件中的代码段已提取')
-        return
-    md = open(fname, encoding='utf8').read()
-    md, pres = extreact_pre(md)
-    open(fname, 'w', encoding='utf8').write(md)
-    open(json_fname, 'w', encoding='utf8').write(json.dumps(pres))
-    
-def recover_pre_handler(args):
-    fname = args.fname
-    if not fname.endswith('.md'):
-        print('请提供MD文件')
-        return
-    json_fname = fname + '.json'
-    if not path.isfile(json_fname):
-        print('找不到已提取的代码段')
-        return
-    md = open(fname, encoding='utf8').read()
-    pres = json.loads(open(json_fname, encoding='utf8').read())
-    md = recover_pre(md, pres)
-    open(fname, 'w', encoding='utf8').write(md)
-    os.unlink(json_fname)
     
 def md_align_handler(args):
     fname1 = args.en
