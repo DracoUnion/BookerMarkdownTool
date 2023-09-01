6b010df1a417771c308358c0cab5542e9118c62a
wizardforcel <wizard.z@qq.com>
2023-09-01 16:53:55 +0800

    2023-09-01 16:53:55

--- BookerMarkdownTool/__main__.py
+++ BookerMarkdownTool/__main__.py
@@ -116,6 +116,7 @@ def main():
     comment_parser = subparsers.add_parser("code-comment", help="add comment to code")
     comment_parser.add_argument("fname", help="file or dir name")
     comment_parser.add_argument("-l", "--prompt", default=CODE_COMMENT_PROMPT, help="prompt used for code comment")
+    comment_parser.add_argument("-m", "--model", default='chatglm-ggml-6b-q4_0', help="model name or path")
     comment_parser.set_defaults(func=code_comment_handle)
 
     args = parser.parse_args()
