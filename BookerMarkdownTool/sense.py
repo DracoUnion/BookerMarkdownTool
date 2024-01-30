de6bcfcb39d544a4ac7b3f0d7b25707d7ac94184
wizardforcel <wizard.z@qq.com>
2024-01-30 14:39:58 +0800

    2024-01-30 14:39:58

--- BookerMarkdownTool/sense.py
+++ BookerMarkdownTool/sense.py
@@ -12,7 +12,7 @@ def filter_sense_file(args):
         
     wdli_fname = path.join(DIR, 'asset', 'TencentSensitiveWords.zip')
     zip = zipfile.ZipFile(BytesIO(open(wdli_fname, 'rb').read()))
-    wdli = zip.read('TencentSensitiveWords.txt').decode('utf8', 'ignore').split('\n'))
+    wdli = zip.read('TencentSensitiveWords.txt').decode('utf8', 'ignore').split('\n')
     wdli = [w for w in wdli if w.strip()]
     
     cont = open(args.fname, encoding='utf8').read()
