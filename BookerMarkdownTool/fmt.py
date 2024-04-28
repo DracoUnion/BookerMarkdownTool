8e9f91805bbdea7a70f75a8019b81b3d8aaf4f6e
wizardforcel <wizard.z@qq.com>
2024-04-28 16:37:24 +0800

    2024-04-28 16:37:24

--- BookerMarkdownTool/fmt.py
+++ BookerMarkdownTool/fmt.py
@@ -81,11 +81,18 @@ def fmt_img(text):
     return re.sub(RE_INNER_IMG, repl_img_func, text, flags=re.VERBOSE)
 
 def fmt_chnum(text):
-    return re.sub(
+    text =  re.sub(
         r'第\x20*(\d{1,4})\x20*章',
         lambda g: '第' + num4d_to_zh(int(g.group(1))) + '章',
         text, 
     )
+    text =  re.sub(r'(# 第.+?章)(?=。|\.\x20*)', r'\1', text)
+    text =  re.sub(
+        r'第\x20*(\d{1,4})\x20*(?:部分|节)',
+        lambda g: '第' + num4d_to_zh(int(g.group(1))) + '部分',
+        text, 
+    )
+    return text
 
 def fmt_title_num(text):
     title, pos = get_md_title(text)
