01e8bde0f8b7494a2c519efe5d15c2aadbe9e358
wizardforcel <wizard.z@qq.com>
2024-05-27 12:31:26 +0800

    Revert "2024-05-27 12:30:54"
    This reverts commit f6fe9e04f5043634454337d81339923589f864c3.

--- BookerMarkdownTool/tomd.py
+++ BookerMarkdownTool/tomd.py
@@ -56,4 +56,71 @@ def tomd(html, lang=None):
     if lang:
         md = re.sub(r'```([\s\S]+?```)', '```' + lang + r'\1', md)
     return md
-  
+    
+def download_handle(args):
+    crawl_cfg['proxy'] = args.proxy
+    crawl_cfg['retry'] = args.retry
+    html = request_retry(
+        'GET', args.url,
+        headers=default_hdrs,
+        retry=args.retry,
+        proxies={'http': args.proxy, 'https': args.proxy},
+    ).content.decode(args.encoding, 'ignore')
+    
+    # 解析标题
+    rt = pq(html)
+    el_title = rt.find(args.title).eq(0)
+    title = el_title.text().strip()
+    el_title.remove()
+    
+    # 判断是否重复
+    title_esc = re.sub(r'[^0-9a-zA-Z_\u4e00-\u9fff]', '-', title)
+    fname = f'docs/{title_esc}.md'
+    if path.isfile(fname):
+        print(f'{title} 已存在')
+        return
+    
+    # 解析内容并下载图片
+    if args.remove:
+        rt('args.remove').remove()
+    if args.body:
+        co = rt.find(args.body).html()
+    else:
+        co = Document(str(rt)).summary()
+        co = pq(co).find('body').html()
+    if not co: 
+        print('未获取到内容！')
+        return 
+    if args.img_src:
+        crawl_cfg['imgSrc'] = args.img_src.split(',')
+    imgs = {}
+    co = process_img(co, imgs, img_prefix='img/', page_url=args.url)
+    html = f'''
+    <html><body>
+    <h1>{title}</h1>
+    <blockquote>
+    来源：<a href='{args.url}'>{args.url}</a>
+    </blockquote>
+    {co}</body></html>
+    '''
+    
+    # 转换 md
+    md = tomd(html)
+    # md = re.sub(RE_CODE_BLOCK, code_replace_func, md)
+    yaml_head = '\n'.join([
+        '<!--yml',
+        'category: ' + args.category,
+        'date: ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        '-->',
+    ])
+    md = f'{yaml_head}\n\n{md}'
+    
+    # 写入硬盘
+    safe_mkdir('docs')
+    safe_mkdir('docs/img')
+    open(fname, 'w', encoding='utf-8').write(md)
+    for name, data in imgs.items():
+        open(f'docs/img/{name}', 'wb').write(data)
+        
+    print('已完成')
+
