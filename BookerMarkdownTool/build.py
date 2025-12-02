385079dd0bd5d7874267eb4c09d980693a06e0cb
wizardforcel <562826179@qq.com>
2025-12-02 18:39:23 +0800

    2025-12-02 18:39:23

--- BookerMarkdownTool/build.py
+++ BookerMarkdownTool/build.py
@@ -2,6 +2,7 @@ from .util import *
 from pyquery import PyQuery as pq
 from os import path
 from typing import *
+from GenEpub import gen_epub
 
 def get_toc(md: str, base: str) -> List[str]:
     html = md2html_pandoc(md)
@@ -35,6 +36,7 @@ def process_img(html: str, base: str, imgs: Dict[str, bytes]) -> str:
         fname = el.attr('src')
         if re.search('^https?://', fname):
             continue
+        print(fname)
         fname = path.join(base, fname)
         if not path.isfile(fname):
             print(f'{fname} 未找到')
@@ -45,3 +47,41 @@ def process_img(html: str, base: str, imgs: Dict[str, bytes]) -> str:
         el.attr('src', nfname)
 
     return str(rt)
+
+def build(args):
+    args.dir = path.abspath(args.dir)
+    if not path.isdir(args.dir):
+        print('请提供文档目录！')
+        return 
+    docrt = os.listdir(args.dir)
+    if 'README.md' not in docrt or \
+       'SUMMARY.md' not in docrt:
+       print('请提供文档目录！')
+       return
+
+    summary = open(path.join(args.dir, 'SUMMARY.md'), encoding='utf8').read()
+    toc = get_toc(summary, args.dir)
+    articles = []
+    imgs = {}
+    for fname in toc:
+        print(fname)
+        md = open(fname, encoding='utf8').read()
+        art = get_article(md)
+        art['content'] = process_img(
+             art['content'], 
+             path.dirname(fname),
+             imgs,
+        )
+        articles.append(art)
+    
+    readme = open(path.join(args.dir, 'README.md'), encoding='utf8').read()
+    readme_html = md2html_pandoc(readme)
+    title = pq(readme_html).find('h1, h2, h3').eq(0).text().strip()
+    if not title:
+        title = path.basename(args.dir)
+    epub_fname = path.join(
+        path.dirname(args.dir),
+        fname_escape(title) + '.epub'
+    )
+    gen_epub(articles, imgs, None, epub_fname)
+    print(epub_fname)
