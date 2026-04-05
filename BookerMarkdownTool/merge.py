173b47745cec81d49f70aed0033a7ac3830c3188
wizardforcel <562826179@qq.com>
2026-04-05 09:57:43 +0800

    2026-04-05 09:57:43

--- BookerMarkdownTool/merge.py
+++ BookerMarkdownTool/merge.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 from .util import *
+from .chunker import chunk_markdown
 
 def merge(args):
     dir = path.abspath(args.dir)
@@ -23,20 +24,26 @@ def merge(args):
         open(f, encoding='utf8', errors='ignore').read() 
         for f in fnames
     ]
-    for i in range(0, len(mds) - 1):
-        if mds[i].count('\n') < args.lines:
-            mds[i+1] = mds[i] + '\n\n' + mds[i+1]
-            mds[i] = ''
+
+    md = '\n\n'.join(mds)
+    cres = chunk_markdown(md, path.basename(dir))
+    chunks = [c.content for c in cres.chunks]
+    groups = ['']
+    for c in chunks:
+        if groups[-1].count('\n') < args.lines:
+            groups[-1] += '\n\n' + c
+        else:
+            groups.append(c)
             
-    mds = [md for md in mds if md]
+    groups = [g for g in groups if g]
 
     # 批量替换图像前缀
     if args.img_pref:
         if not args.img_pref.endswith('/'):
             args.img_pref += '/'
-        mds = [
-            re.sub(r'(?<=\]\()img/', args.img_pref, md)
-            for md in mds
+        groups = [
+            re.sub(r'(?<=\]\()img/', args.img_pref, g)
+            for g in groups
         ]
 
     # 未设置标题情况下从 README 里面读取标题并设置
@@ -47,12 +54,12 @@ def merge(args):
         args.title, _ = get_md_title(readme)
         credit = get_md_credit(readme)
 
-    l = len(str(len(mds)))
-    for i, md in enumerate(mds):
+    l = len(str(len(groups)))
+    for i, g in enumerate(groups):
         fname = dir + '-merge-' + str(i).zfill(l) + '.md'
         print(fname)
         title = f'# {args.title}（{num4d_to_zh(i + 1)}）'
-        md = f'{title}\n\n{credit}\n\n{md}'
-        open(fname, 'w', encoding='utf8').write(md)
+        g = f'{title}\n\n{credit}\n\n{g}'
+        open(fname, 'w', encoding='utf8').write(g)
     
             
